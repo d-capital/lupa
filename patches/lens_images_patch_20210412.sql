030a61d1b0eb33c3d624345ab3e6b2d0b5ce3c35
d-capital <dzyuba.stanislaw@mail.ru>
2021-04-12 22:26:32 +0300

    patch updated

---
+++ patches/lens_images_patch_20210412.sql
@@ -0,0 +1,9 @@
+INSERT INTO lens_images (brand,color_name, image_path, display_order)
+VALUES ('Medeo','Ring blue',
+'\static\images\lens_images\medeo\ring_blue\2.jpg',2);
+INSERT INTO lens_images (brand,color_name, image_path, display_order)
+VALUES ('Medeo','Ring blue',
+'\static\images\lens_images\medeo\ring_blue\3.jpg',3);
+INSERT INTO lens_images (brand,color_name, image_path, display_order)
+VALUES ('Medeo','Ring blue',
+'\static\images\lens_images\medeo\ring_blue\4.jpg',4);
