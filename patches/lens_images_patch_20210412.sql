e546223054c614f50c9264fb5838cd6b909d960a
d-capital <dzyuba.stanislaw@mail.ru>
2021-04-14 09:10:20 +0300

    patch updated

--- patches/lens_images_patch_20210412.sql
+++ patches/lens_images_patch_20210412.sql
@@ -1,9 +1,219 @@
 INSERT INTO lens_images (brand,color_name, image_path, display_order)
-VALUES ('Medeo','Ring blue',
-'\static\images\lens_images\medeo\ring_blue\2.jpg',2);
+VALUES ('Alba Colors','Black',
+'\static\images\lens_images\alba_colors\black\2.jpg',2);
+
 INSERT INTO lens_images (brand,color_name, image_path, display_order)
-VALUES ('Medeo','Ring blue',
-'\static\images\lens_images\medeo\ring_blue\3.jpg',3);
+VALUES ('Alba Colors','Black',
+'\static\images\lens_images\alba_colors\black\3.jpg',3);
+
 INSERT INTO lens_images (brand,color_name, image_path, display_order)
-VALUES ('Medeo','Ring blue',
-'\static\images\lens_images\medeo\ring_blue\4.jpg',4);
+VALUES ('Alba Colors','Black',
+'\static\images\lens_images\alba_colors\black\4.jpg',4);
+
+INSERT INTO lens_images (brand,color_name, image_path, display_order)
+VALUES ('Alba Colors','Blue Marine',
+'\static\images\lens_images\alba_colors\blue_marine\2.jpg',2);
+
+INSERT INTO lens_images (brand,color_name, image_path, display_order)
+VALUES ('Alba Colors','Blue Marine',
+'\static\images\lens_images\alba_colors\blue_marine\3.jpg',3);
+
+INSERT INTO lens_images (brand,color_name, image_path, display_order)
+VALUES ('Alba Colors','Blue Marine',
+'\static\images\lens_images\alba_colors\blue_marine\4.jpg',4);
+
+INSERT INTO lens_images (brand,color_name, image_path, display_order)
+VALUES ('Alba Colors','Caramel',
+'\static\images\lens_images\alba_colors\caramel\2.jpg',2);
+
+INSERT INTO lens_images (brand,color_name, image_path, display_order)
+VALUES ('Alba Colors','Caramel',
+'\static\images\lens_images\alba_colors\caramel\3.jpg',3);
+
+INSERT INTO lens_images (brand,color_name, image_path, display_order)
+VALUES ('Alba Colors','Caramel',
+'\static\images\lens_images\alba_colors\caramel\4.jpg',4);
+
+INSERT INTO lens_images (brand,color_name, image_path, display_order)
+VALUES ('Medeo','Ice White',
+'\static\images\lens_images\medeo\ice_white\2.jpg',2);
+
+INSERT INTO lens_images (brand,color_name, image_path, display_order)
+VALUES ('Medeo','Ice White',
+'\static\images\lens_images\medeo\ice_white\3.jpg',3);
+
+INSERT INTO lens_images (brand,color_name, image_path, display_order)
+VALUES ('Medeo','Ice White',
+'\static\images\lens_images\medeo\ice_white\4.jpg',4);
+
+INSERT INTO lens_images (brand,color_name, image_path, display_order)
+VALUES ('Medeo','Iceberg',
+'\static\images\lens_images\medeo\iceberg\2.jpg',2);
+
+INSERT INTO lens_images (brand,color_name, image_path, display_order)
+VALUES ('Medeo','Iceberg',
+'\static\images\lens_images\medeo\iceberg\3.jpg',3);
+
+INSERT INTO lens_images (brand,color_name, image_path, display_order)
+VALUES ('Medeo','Iceberg',
+'\static\images\lens_images\medeo\iceberg\4.jpg',4);
+
+INSERT INTO lens_images (brand,color_name, image_path, display_order)
+VALUES ('Medeo','Icy',
+'\static\images\lens_images\medeo\icy\2.jpg',2);
+
+INSERT INTO lens_images (brand,color_name, image_path, display_order)
+VALUES ('Medeo','Icy',
+'\static\images\lens_images\medeo\icy\3.jpg',3);
+
+INSERT INTO lens_images (brand,color_name, image_path, display_order)
+VALUES ('Medeo','Quartz',
+'\static\images\lens_images\medeo\quartz\2.jpg',2); 
+
+INSERT INTO lens_images (brand,color_name, image_path, display_order)
+VALUES ('Medeo','Ring gray',
+'\static\images\lens_images\medeo\ring_gray\2.jpg',2);
+
+INSERT INTO lens_images (brand,color_name, image_path, display_order)
+VALUES ('Medeo','Ring green',
+'\static\images\lens_images\medeo\ring_green\2.jpg',2);
+
+INSERT INTO lens_images (brand,color_name, image_path, display_order)
+VALUES ('Medeo','Ring green',
+'\static\images\lens_images\medeo\ring_green\3.jpg',3);
+
+INSERT INTO lens_images (brand,color_name, image_path, display_order)
+VALUES ('Medeo','Ring green',
+'\static\images\lens_images\medeo\ring_green\4.jpg',4);
+
+INSERT INTO lens_images (brand,color_name, image_path, display_order)
+VALUES ('Medeo','Ring Olive',
+'\static\images\lens_images\medeo\ring_olive\2.jpg',2);
+
+INSERT INTO lens_images (brand,color_name, image_path, display_order)
+VALUES ('Medeo','Ring Olive',
+'\static\images\lens_images\medeo\ring_olive\3.jpg',3);
+
+INSERT INTO lens_images (brand,color_name, image_path, display_order)
+VALUES ('Medeo','Wild blue',
+'\static\images\lens_images\medeo\wild_blue\2.jpg',2);
+
+INSERT INTO lens_images (brand,color_name, image_path, display_order)
+VALUES ('Medeo','Wild blue',
+'\static\images\lens_images\medeo\wild_blue\3.jpg',3);
+
+INSERT INTO lens_images (brand,color_name, image_path, display_order)
+VALUES ('Medeo','Wild green',
+'\static\images\lens_images\medeo\wild_green\2.jpg',2);
+
+INSERT INTO lens_images (brand,color_name, image_path, display_order)
+VALUES ('Medeo','Wild green',
+'\static\images\lens_images\medeo\wild_green\3.jpg',3);
+
+INSERT INTO lens_images (brand,color_name, image_path, display_order)
+VALUES ('Urban Layer','Cloud Blue',
+'\static\images\lens_images\urban_layer\cloud_blue\2.jpg',2);
+
+INSERT INTO lens_images (brand,color_name, image_path, display_order)
+VALUES ('Urban Layer','Cloud Blue',
+'\static\images\lens_images\urban_layer\cloud_blue\3.jpg',3);
+
+INSERT INTO lens_images (brand,color_name, image_path, display_order)
+VALUES ('Urban Layer','Cloud Blue',
+'\static\images\lens_images\urban_layer\cloud_blue\4.jpg',4);
+
+INSERT INTO lens_images (brand,color_name, image_path, display_order)
+VALUES ('Urban Layer','Cloud Gray',
+'\static\images\lens_images\urban_layer\cloud_gray\2.jpg',2);
+
+INSERT INTO lens_images (brand,color_name, image_path, display_order)
+VALUES ('Urban Layer','Cloud Gray',
+'\static\images\lens_images\urban_layer\cloud_gray\3.jpg',3);
+
+INSERT INTO lens_images (brand,color_name, image_path, display_order)
+VALUES ('Urban Layer','Cloud Gray',
+'\static\images\lens_images\urban_layer\cloud_gray\4.jpg',4);
+
+INSERT INTO lens_images (brand,color_name, image_path, display_order)
+VALUES ('Urban Layer','Cloud Green',
+'\static\images\lens_images\urban_layer\cloud_green\2.jpg',2);
+
+INSERT INTO lens_images (brand,color_name, image_path, display_order)
+VALUES ('Urban Layer','Cloud Green',
+'\static\images\lens_images\urban_layer\cloud_green\3.jpg',3);
+
+INSERT INTO lens_images (brand,color_name, image_path, display_order)
+VALUES ('Urban Layer','Cloud Green',
+'\static\images\lens_images\urban_layer\cloud_green\4.jpg',4);
+
+INSERT INTO lens_images (brand,color_name, image_path, display_order)
+VALUES ('Urban Layer','Cloud R Blue',
+'\static\images\lens_images\urban_layer\cloud_r_blue\2.jpg',2);
+
+INSERT INTO lens_images (brand,color_name, image_path, display_order)
+VALUES ('Urban Layer','Cloud R Blue',
+'\static\images\lens_images\urban_layer\cloud_r_blue\3.jpg',3);
+
+INSERT INTO lens_images (brand,color_name, image_path, display_order)
+VALUES ('Urban Layer','Cloud R Blue',
+'\static\images\lens_images\urban_layer\cloud_r_blue\4.jpg',4);
+
+INSERT INTO lens_images (brand,color_name, image_path, display_order)
+VALUES ('Urban Layer','Cloud R Gray',
+'\static\images\lens_images\urban_layer\cloud_r_gray\2.jpg',2);
+
+INSERT INTO lens_images (brand,color_name, image_path, display_order)
+VALUES ('Urban Layer','Cloud R Gray',
+'\static\images\lens_images\urban_layer\cloud_r_gray\3.jpg',3);
+
+INSERT INTO lens_images (brand,color_name, image_path, display_order)
+VALUES ('Urban Layer','Cloud R Gray',
+'\static\images\lens_images\urban_layer\cloud_r_gray\4.jpg',4);
+
+INSERT INTO lens_images (brand,color_name, image_path, display_order)
+VALUES ('Urban Layer','Cloud R Green',
+'\static\images\lens_images\urban_layer\cloud_r_green\2.jpg',2);
+
+INSERT INTO lens_images (brand,color_name, image_path, display_order)
+VALUES ('Urban Layer','Cloud R Green',
+'\static\images\lens_images\urban_layer\cloud_r_green\3.jpg',3);
+
+INSERT INTO lens_images (brand,color_name, image_path, display_order)
+VALUES ('Urban Layer','Sofia Aqua',
+'\static\images\lens_images\urban_layer\sofia_aqua\2.jpg',2);
+
+INSERT INTO lens_images (brand,color_name, image_path, display_order)
+VALUES ('Urban Layer','Sofia Aqua',
+'\static\images\lens_images\urban_layer\sofia_aqua\3.jpg',3);
+
+INSERT INTO lens_images (brand,color_name, image_path, display_order)
+VALUES ('Urban Layer','Sofia Aqua',
+'\static\images\lens_images\urban_layer\sofia_aqua\4.jpg',4);
+
+INSERT INTO lens_images (brand,color_name, image_path, display_order)
+VALUES ('Urban Layer','Sofia Gray',
+'\static\images\lens_images\urban_layer\sofia_gray\2.jpg',2);
+
+INSERT INTO lens_images (brand,color_name, image_path, display_order)
+VALUES ('Urban Layer','Sofia Gray',
+'\static\images\lens_images\urban_layer\sofia_gray\3.jpg',3);
+
+INSERT INTO lens_images (brand,color_name, image_path, display_order)
+VALUES ('Urban Layer','Sofia Gray',
+'\static\images\lens_images\urban_layer\sofia_gray\4.jpg',4);
+
+INSERT INTO lens_images (brand,color_name, image_path, display_order)
+VALUES ('Urban Layer','Cloud Choco',
+'\static\images\lens_images\urban_layer\cloud_choco\1.jpg',1);
+
+INSERT INTO lens_images (brand,color_name, image_path, display_order)
+VALUES ('Urban Layer','Cloud Choco',
+'\static\images\lens_images\urban_layer\cloud_choco\2.jpg',2);
+
+INSERT INTO lens_images (brand,color_name, image_path, display_order)
+VALUES ('Urban Layer','Cloud Choco',
+'\static\images\lens_images\urban_layer\cloud_choco\3.jpg',3);
+
+INSERT INTO lens_images (brand,color_name, image_path, display_order)
+VALUES ('Urban Layer','Cloud R Choco',
+'\static\images\lens_images\urban_layer\cloud_r_choco\1.jpg',1);
