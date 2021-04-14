8127f561c8c9851b8c41d625b428d111b041250d
d-capital <dzyuba.stanislaw@mail.ru>
2021-04-14 23:11:40 +0300

    patch update

---
+++ patches/lenses_patch_20210414.sql
@@ -0,0 +1,6 @@
+INSERT INTO lenses (brand,color_name, dioptrics,
+color_category,unit,price,stock,replacement_frequency)
+VALUES ('Urban Layer','Cloud R Choco',0.0,'brown','pcs',1500,0,'annual');
+INSERT INTO lenses (brand,color_name, dioptrics,
+color_category,unit,price,stock,replacement_frequency)
+VALUES ('Urban Layer','Cloud Choco',0.0,'brown','pcs',1500,0,'annual');
